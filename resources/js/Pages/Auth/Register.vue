321635ad2c40054dac1e70bfa445636b3411033c
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-10-11 18:00:12 +0800

    added register attributes and data

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -14,6 +14,17 @@ const form = useForm({
     password: '',
     password_confirmation: '',
     terms: false,
+    civil_status:'',
+    gender:'',
+    college:'',
+    course:'',
+    adress:'',
+    phone_number:'',
+    current_work:'',
+    year_graduated:'',
+    motto:'',
+    nickname:'',
+
 });
 
 const submit = () => {
@@ -82,6 +93,136 @@ const submit = () => {
                     autocomplete="new-password"
                 />
                 <InputError class="mt-2" :message="form.errors.password_confirmation" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="civil_status" value="Civil Status" />
+                <select
+                    id="civil_status"
+                    v-model="form.civil_status"
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                >
+                    <option value="1">Single</option>
+                    <option value="2">Married</option>
+                    <option value="3">Annulled</option>
+                    <option value="4">Widowed</option>
+                </select>
+                <InputError class="mt-2" :message="form.errors.civil_status" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="gender" value="Gender" />
+                <select
+                    id="gender"
+                    v-model="form.gender"
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                >
+                    <option value="1">Male</option>
+                    <option value="2">Female</option>
+                </select>
+                <InputError class="mt-2" :message="form.errors.gender" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="college" value="College" />
+                <select
+                    id="college"
+                    v-model="form.college"
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                >
+                    <option value="1">College of Engineering</option>
+                    <option value="1">College of Agriculture</option>
+                    <option value="1">College of Arts and Sciences</option>
+                </select>
+                <InputError class="mt-2" :message="form.errors.college_error" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="course" value="Course" />
+                <select
+                    id="course"
+                    v-model="form.course"
+                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                >
+                    <option value="1">Batchelor of Science in Information Technology</option>
+                    <option value="1">Batchelor of Science in Civil Engineering</option>
+                    <option value="1">Batchelor of Science in Mechanichal Engineering</option>
+                </select>
+                <InputError class="mt-2" :message="form.errors.course" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="address" value="Address" />
+                <TextInput
+                    id="address"
+                    v-model="form.address"
+                    type="text"
+                    class="mt-1 block w-full"
+                    required
+                />
+                <InputError class="mt-2" :message="form.errors.address" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="phone_number" value="Phone / Mobile #" />
+                <TextInput
+                    id="phone_number"
+                    v-model="form.phone_number"
+                    type="text"
+                    class="mt-1 block w-full"
+                    required
+                />
+                <InputError class="mt-2" :message="form.errors.phone_number" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="current_work" value="Current work" />
+                <TextInput
+                    id="current_work"
+                    v-model="form.current_work"
+                    type="text"
+                    class="mt-1 block w-full"
+                    required
+                />
+                <InputError class="mt-2" :message="form.errors.current_work" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="year_graduated" value="Year Graduated" />
+                <TextInput
+                    id="year_graduated"
+                    v-model="form.year_graduated"
+                    type="text"
+                    class="mt-1 block w-full"
+                    required
+                />
+                <InputError class="mt-2" :message="form.errors.year_graduated" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="motto" value="Motto" />
+                <TextInput
+                    id="motto"
+                    v-model="form.motto"
+                    type="text"
+                    class="mt-1 block w-full border break-word"
+                    style="height:4vmin"
+                    required
+                />
+                <InputError class="mt-2" :message="form.errors.motto" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="nickname" value="Nickname" />
+                <TextInput
+                    id="nickname"
+                    v-model="form.nickname"
+                    type="text"
+                    class="mt-1 block w-full border break-word"
+                    style="height:4vmin"
+                    required
+                />
+                <InputError class="mt-2" :message="form.errors.nickname" />
             </div>
 
             <div v-if="$page.props.jetstream.hasTermsAndPrivacyPolicyFeature" class="mt-4">
