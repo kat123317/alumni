1e8b926c604740a962a900a107020ea65ece5820
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-10-12 22:02:57 +0800

    added controllers and dashboard data configurations

--- resources/js/Pages/Administrator/Index.vue
+++ resources/js/Pages/Administrator/Index.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { ref, onMounted } from 'vue'
+import route from '../../../../vendor/tightenco/ziggy/src/js';
 
 const trigger  = ref(1);
 
+const backDashboard = () => {
+    route('dashnoard')
+}
 </script>
 <template>
     <div>
@@ -18,7 +22,7 @@ const trigger  = ref(1);
                 <button @click="trigger = 3" class="mr-5 text-white hover:text-gray-200">User Managenment</button>
                 <button @click="trigger = 4" class="mr-5 text-white hover:text-gray-200">Survey</button>
                 </nav>
-                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Back to home
+                <button @click="backDashboard()" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Back to home
                 <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                     <path d="M5 12h14M12 5l7 7-7 7"></path>
                 </svg>
