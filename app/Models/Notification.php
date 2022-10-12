bf38412d6e5efc752c0da12247e8b9acc7895c2e
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-10-12 18:11:54 +0800

    added migrations

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -11,4 +11,9 @@ class Notification extends Model
     use HasFactory, SoftDeletes;
 
     protected $guarded = [];
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
 import Welcome from '@/Components/Welcome.vue';
+import { Head, Link, useForm, usePage } from '@inertiajs/inertia-vue3';
 import { ref, onMounted } from 'vue'
 
 const option_view  = ref(true)
@@ -21,7 +22,7 @@ const option_view  = ref(true)
             <nav class="flex mb-5" aria-label="Breadcrumb">
                 <ol class="inline-flex items-center space-x-1 md:space-x-3">
                     <li class="inline-flex items-center">
-                    <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+                    <a href="#" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-gray-900">
                         <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
                         Announcements
                     </a>
@@ -34,21 +35,21 @@ const option_view  = ref(true)
                     <!-- <Welcome /> -->
                     <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-1 ...">   
                     <div>
-                        <div class=" w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                            <time class="text-lg font-semibold text-gray-900 dark:text-white">Notifications</time>
-                            <ol class="mt-3 divide-y max-h-[40vmin] overflow-hidden overflow-y-auto divider-gray-200 dark:divide-gray-700">
-                                <li v-for="items in 10">
-                                    <a href="#" class="block items-center p-3 sm:flex hover:bg-gray-100 dark:hover:bg-gray-700">
-                                        <img class="mr-3 mb-3 w-12 h-12 rounded-full sm:mb-0" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Central_Mindanao_University_logo.png/180px-Central_Mindanao_University_logo.png" alt="Jese Leos image">
-                                        <div class="text-gray-600 dark:text-gray-400">
-                                            <div class="text-base font-normal"><span class="font-medium text-gray-900 dark:text-white">College of Engineering</span>
-                                                <span class="inline-flex items-center ml-2 text-xs font-normal text-gray-500 dark:text-gray-400">
+                        <div class=" w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8">
+                            <time class="text-lg font-semibold text-gray-900">Notifications</time>
+                            <ol class="mt-3 divide-y max-h-[40vmin] overflow-hidden overflow-y-auto divider-gray-200">
+                                <li v-for="(notifications, key) in usePage().props.value.notifications" :key="key">
+                                    <a href="#" class="block items-center p-3 sm:flex hover:bg-gray-100">
+                                        <img class="mr-3 mb-3 w-12 h-12 rounded-full sm:mb-0" src="shttps://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Central_Mindanao_University_logo.png/180px-Central_Mindanao_University_logo.png" alt="Jese Leos image">
+                                        <div class="text-gray-600">
+                                            <div class="text-base font-normal"><span class="font-medium text-gray-900">College of Engineering</span>
+                                                <span class="inline-flex items-center ml-2 text-xs font-normal text-gray-500">
                                                 <svg aria-hidden="true" class="mr-1 w-3 h-3" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.083 9h1.946c.089-1.546.383-2.97.837-4.118A6.004 6.004 0 004.083 9zM10 2a8 8 0 100 16 8 8 0 000-16zm0 2c-.076 0-.232.032-.465.262-.238.234-.497.623-.737 1.182-.389.907-.673 2.142-.766 3.556h3.936c-.093-1.414-.377-2.649-.766-3.556-.24-.56-.5-.948-.737-1.182C10.232 4.032 10.076 4 10 4zm3.971 5c-.089-1.546-.383-2.97-.837-4.118A6.004 6.004 0 0115.917 9h-1.946zm-2.003 2H8.032c.093 1.414.377 2.649.766 3.556.24.56.5.948.737 1.182.233.23.389.262.465.262.076 0 .232-.032.465-.262.238-.234.498-.623.737-1.182.389-.907.673-2.142.766-3.556zm1.166 4.118c.454-1.147.748-2.572.837-4.118h1.946a6.004 6.004 0 01-2.783 4.118zm-6.268 0C6.412 13.97 6.118 12.546 6.03 11H4.083a6.004 6.004 0 002.783 4.118z" clip-rule="evenodd"></path></svg>
                                                 Public
                                             </span> 
                                          </div>
                                          <div class="text-sm font-normal">"COE palaro Meeting"</div>
-                                         <button type="button" class="text-white  text-xs bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Show More</button>
+                                         <button type="button" class="text-white  text-xs bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Show More</button>
                                         </div>
                                     </a>
                                 </li>
@@ -56,27 +57,27 @@ const option_view  = ref(true)
                             
                      
                         </div>
-                        <div class="p-4 mt-10 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                        <div class="p-4 mt-10 w-full max-w-md bg-white rounded-lg border shadow-md sm:p-8">
                             <div class="flex justify-between items-center mb-4">
-                                <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Colleges</h5>
+                                <h5 class="text-xl font-bold leading-none text-gray-900 ">Colleges</h5>
                             </div>
                                 <div class="flow-root">
-                                    <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-                                        <li v-for="ïtems in 4" class="py-3 sm:py-4">
+                                    <ul role="list" class="divide-y divide-gray-200">
+                                        <li v-for="(colleges, key) in usePage().props.value.colleges" :key="key" class="py-3 sm:py-4">
                                             <div class="flex items-center space-x-4">
                                                 <div class="flex-shrink-0">
-                                                    <img class="w-8 h-8 rounded-full" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Central_Mindanao_University_logo.png/180px-Central_Mindanao_University_logo.png" alt="Neil image">
+                                                    <img class="w-8 h-8 rounded-full" src="shttps://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Central_Mindanao_University_logo.png/180px-Central_Mindanao_University_logo.png" alt="Neil image">
                                                 </div>
                                                 <div class="flex-1 min-w-0">
-                                                    <p class="text-sm font-medium text-gray-900 truncate dark:text-white">
-                                                        College of Information Technology
+                                                    <p class="text-sm font-medium text-gray-900 truncate" :title="colleges.name">
+                                                        College of {{ colleges.name }}
                                                     </p>
                                                     <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                                        BSIT@windster.com
+                                                        {{ colleges.abbreviation }}
                                                     </p>
                                                 </div>
-                                                <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                                    <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                                <div class="inline-flex items-center text-base font-semibold text-gray-900 ">
+                                                    <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center">
                                                         Show
                                                         <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                                                     </button>
@@ -88,17 +89,17 @@ const option_view  = ref(true)
                                 </div>
                             </div>
                         </div>
-                            <div class="col-span-2 w-full  bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700 px-10">
-                                <time class="text-lg font-semibold  text-gray-900 dark:text-white">Announcements</time>
+                            <div class="col-span-2 w-full  bg-white rounded-lg border shadow-md sm:p-8 px-10">
+                                <time class="text-lg font-semibold  text-gray-900 ">Announcements</time>
                                 <form class="flex mt-5 items-center">   
                                     <label for="simple-search" class="sr-only">Search</label>
                                     <div class="relative w-full">
                                         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                                         </div>
-                                        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Search" required>
+                                        <input type="text" id="simple-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full pl-10 p-2.5" placeholder="Search" required>
                                     </div>
-                                    <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+                                    <button type="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300">
                                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                                         <span class="sr-only">Search</span>
                                     </button>
@@ -106,16 +107,16 @@ const option_view  = ref(true)
                                 
 
                                 
-                                <ol class="relative mt-10 border-b  z-20 overflow-auto max-h-[120vmin]   dark:border-gray-700">
+                                <ol class="relative mt-10 border-b  z-20 overflow-auto max-h-[120vmin]">
                                  
-                                    <li  v-for="announcements in 10" class="mb-5 px-10 border-b-1 bg-gray-100 rounded p-5 border-gray-500">
+                                    <li  v-for="(announcements, key) in 10" :key="key" class="mb-5 px-10 border-b-1 bg-gray-100 rounded p-5 border-gray-500">
                                         <div class="flex ">             
-                                              <img width="35" class="mr-2" src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Central_Mindanao_University_logo.png/180px-Central_Mindanao_University_logo.png" alt="">
-                                              <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">College of Education<span class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-200 dark:text-green-800 ml-3">Just Now</span></h3>
+                                              <img width="35" class="mr-2" src="shttps://upload.wikimedia.org/wikipedia/en/thumb/a/a0/Central_Mindanao_University_logo.png/180px-Central_Mindanao_University_logo.png" alt="">
+                                              <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900 ">College of Education<span class="bg-green-100 text-green-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded ml-3">Just Now</span></h3>
                                         </div>
                                       
-                                        <time class="block mb-5 ml-10 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"> January 13th, 2022</time>
-                                        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing pages.</p>
+                                        <time class="block mb-5 ml-10 text-sm font-normal leading-none text-gray-400"> January 13th, 2022</time>
+                                        <p class="mb-4 text-base font-normal text-gray-500">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing pages.</p>
                                         
                                     </li>
                                
