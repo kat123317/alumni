748824acac93da9b3ed1ad9dae340a1eadbd1451
MrRegei <39606446+MrRegei@users.noreply.github.com>
2022-10-20 21:53:30 +0800

    FE
    Announcement

--- resources/js/Pages/Administrator/components/Announcement.vue
+++ resources/js/Pages/Administrator/components/Announcement.vue
@@ -1,8 +1,8 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 
 
-    const collapseVar  = ref(false);
+const collapseVar = ref(false);
     // const checkedData = ref([]);
     // const datasample = ref(2);
     // const courses = ref([
@@ -31,51 +31,96 @@
 
 
 <template>
-   <section class="text-gray-600  h-[90vmin] body-font relative">
-        <div class="absolute  lg:block hidden inset-0 bg-gray-300">
-            <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4700.147561062848!2d125.05044014002942!3d7.859731708811927!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32ff184711ed6b0f%3A0xa6348944e8acd9bf!2sCentral%20Mindanao%20University!5e0!3m2!1sen!2sph!4v1665578365379!5m2!1sen!2sph" style="filter: grayscale(1) contrast(1.2) opacity(0.4);"></iframe>
-            
-        </div>
-        <div class="container px-5 py-24 mx-auto flex">
-            <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col  md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-            <h2 class="text-gray-900 text-lg mb-5 font-medium title-font">Post-Announcement</h2>
-            
-            <!-- <div class="relative mb-4">
-                <label for="email" class="leading-7 text-sm text-gray-600">Privacy</label>
-                <select id="underline_select" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
-                    <option selected>Only Me</option>
-                    <option value="COE">Private</option>
-                    <option value="COED">Public</option>
-                </select>
-            </div> -->
-            <div class="relative w-full  mb-4">
-            <!-- <ul class=" grid grid-cols-2 text-sm  font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 w-100 dark:text-white">
-                <li  v-for="(items, index) in courses" :key="index" class="w-full mr-1 rounded-t-lg border-b dark:border-gray-600">
-                    <div class="flex items-center pl-3">
-                        <input id="vue-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                        <label for="vue-checkbox" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300">{{items.name}}</label>
-                    </div>
-                </li>
-            </ul> -->
-                <label for="message" class="leading-7 text-sm text-gray-600">Title</label>
-                <input id="" type="text" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">
-        
-            </div>
-            <div class="relative mb-4">
-                <label for="message" class="leading-7 text-sm text-gray-600">Announcement</label>
-                <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-            </div>
-            <button class="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Post</button>
-            <p class="text-xs text-gray-500 mt-3">Post some NEWS, events and other important matters.</p>
-            </div>
-        </div>
-        <div class="container px-5 py-24 mx-auto flex">
-            
-            <div class="lg:w-1/2 md:w-1/2 bg-white rounded-lg p-8 flex flex-col  md:ml-auto w-full mt-2 md:mt-0 absolute z-10 shadow-md">
-            <h2 class="text-gray-900 text-lg mb-5 font-medium title-font">Announcement</h2>
-                hello
-            </div>
 
+    <section class="text-gray-600 body-font relative">
+        <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+            <div
+                class="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-center justify-center relative">
+
+
+                <div
+                    class="relative max-w-md mx-auto md:max-w-2xl mt-6 min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-xl mt-16">
+                    <div class="px-6">
+
+                        <div class="text-center mt-5">
+                            <h3 class="text-2xl text-slate-700 font-bold leading-normal mb-1">Announcement Title</h3>
+
+                        </div>
+                        <div class="mt-6 py-6 border-t border-slate-200 text-center">
+                            <div class="flex flex-wrap justify-center">
+                                <div class="w-full px-4">
+                                    <p class="font-light leading-relaxed text-slate-600 mb-4">Announcement Details.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+            </div>
+            <div class="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Post-Announcement</h2>
+                <div class="relative mb-4">
+                    <label for="name" class="leading-7 text-sm text-gray-600">Title</label>
+                    <input type="text" id="name" name="name"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                </div>
+
+                <div class="relative mb-4">
+                    <label for="message" class="leading-7 text-sm text-gray-600">Announcement</label>
+                    <textarea id="message" name="message"
+                        class="w-full bg-white rounded border border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                </div>
+                <button
+                    class="text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">Submit</button>
+            </div>
         </div>
-    </section>  
+
+        <div class="overflow-x-auto flex justify-center  relative">
+            <table class="w-full max-w-7xl  text-sm text-left text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="py-3 px-6">
+                            Product name
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Color
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Category
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Price
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Action
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            Apple MacBook Pro 17"
+                        </th>
+                        <td class="py-4 px-6">
+                            Sliver
+                        </td>
+                        <td class="py-4 px-6">
+                            Laptop
+                        </td>
+                        <td class="py-4 px-6">
+                            $2999
+                        </td>
+                        <td class="py-4 px-6">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </section>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,18 +1,18 @@
 <script setup>
 import { Head, Link, useForm, usePage } from '@inertiajs/inertia-vue3';
-import { ref, onMounted,  computed} from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import moment from 'moment';
 
 const date_conversion = (value) => {
-            if (value) {
-                return moment(value).format('MMMM DD YYYY')
-            }
-        }
+    if (value) {
+        return moment(value).format('MMMM DD YYYY')
+    }
+}
 const date_conversion_complete = (value) => {
-    if(value){
-    return moment(value).format('MMMM Do YYYY, h:mm:ss a');
-  }
-        }
+    if (value) {
+        return moment(value).format('MMMM Do YYYY, h:mm:ss a');
+    }
+}
 
 defineProps({
     canLogin: Boolean,
@@ -74,7 +74,7 @@ defineProps({
                                 </h2>
                                 <p class="text-sm md:text-base text-gray-50 mb-4">Explore your favourite events and
                                     register now to showcase your talent and win exciting prizes.</p>
-                                <a  :href="route('login')"
+                                <a :href="route('login')"
                                     class="bg-transparent hover:bg-green-300 text-green-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-green-300 hover:border-transparent">
                                     Login</a>
                             </div>
@@ -84,7 +84,7 @@ defineProps({
                                         <img class="inline-block mt-28 hidden xl:block"
                                             src="https://s.yimg.com/ny/api/res/1.2/E.XEHApTT5lv1PtuFKk6aQ--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTU0MC42NjY2NjY2NjY2NjY2/https://s.yimg.com/uu/api/res/1.2/bYsw7QtVNbS58wqCg0tvNQ--~B/aD04MTE7dz0xNDQwO2FwcGlkPXl0YWNoeW9u/https://media.zenfs.com/en/cosmopolitan_438/842a18738bf42ecd803a576d6349a8e0">
                                     </div>
-        
+
                                 </div>
                             </div>
                         </div>
@@ -101,43 +101,87 @@ defineProps({
 
 
                     <div class="space-y-8  md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-                        <div class="bg-green-800 border-double border-8 border-white rounded p-2 drop-shadow-sm">
-                            <h3 class="mb-2 text-2xl font-extrabold text-white italic ">Mandate</h3>
-                            <h3 class="mb-2 text-1xl font-extrabold text-white">Section 2 of Republic Act No. 4498
-                                mandates that Central Mindanao University “shall provide programs of instruction at
-                                all levels in the arts, sciences, technical, professional, educational, and
-                                philosophical fields, and shall concern itself with “pure” and “applied” research in
-                                all branches of knowledge for the intellectual and professional growth of faculty
-                                members, for the advance instruction of students, and for increasing knowledge and
-                                understanding.”</h3>
+                        <div class="">
+
+                            <figure class="mx-auto max-w-screen-md text-center">
+                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600"
+                                    viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                                        fill="currentColor"></path>
+                                </svg>
+                                <blockquote>
+                                    <p class="text-2xl italic font-medium text-gray-900 dark:text-white">Section 2 of
+                                        Republic Act No. 4498
+                                        mandates that Central Mindanao University “shall provide programs of instruction
+                                        at
+                                        all levels in the arts, sciences, technical, professional, educational, and
+                                        philosophical fields, and shall concern itself with “pure” and “applied”
+                                        research in
+                                        all branches of knowledge for the intellectual and professional growth of
+                                        faculty
+                                        members, for the advance instruction of students, and for increasing knowledge
+                                        and
+                                        understanding.</p>
+                                </blockquote>
+                                <figcaption class="flex justify-center items-center mt-6 space-x-3">
+
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <cite class="pr-3 font-medium text-gray-900 dark:text-white">Mandate</cite>
+
+                                    </div>
+                                </figcaption>
+                            </figure>
+
                         </div>
-                        <div class="bg-white border-double border-8 border-green-800 rounded p-2 drop-shadow-sm">
-                            <h3 class="mb-2 text-2xl font-extrabold text-green-900 flex italic ">Mission <svg
-                                    class="ml-2 w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253">
-                                    </path>
-                                </svg></h3>
-                            <h3 class="mb-2 text-1xl font-extrabold text-gray-900">To advance the frontier of
-                                knowledge through internationalization of education and equitable access to quality
-                                instruction, research, extension and production for economic prosperity, moral
-                                integrity, social and cultural sensitivity and environmental consciousness.</h3>
+                        <div class="">
+                            <figure class="mx-auto max-w-screen-md text-center">
+                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600"
+                                    viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                                        fill="currentColor"></path>
+                                </svg>
+                                <blockquote>
+                                    <p class="text-2xl italic font-medium text-gray-900 dark:text-white">To advance the
+                                        frontier of
+                                        knowledge through internationalization of education and equitable access to
+                                        quality
+                                        instruction, research, extension and production for economic prosperity, moral
+                                        integrity, social and cultural sensitivity and environmental consciousness.</p>
+                                </blockquote>
+                                <figcaption class="flex justify-center items-center mt-6 space-x-3">
+
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <cite class="pr-3 font-medium text-gray-900 dark:text-white">Mission</cite>
+
+                                    </div>
+                                </figcaption>
+                            </figure>
                         </div>
-                        <div
-                            class="bg-green-800 border-double border-8 border-white border-white rounded  p-2 drop-shadow-sm">
-                            <h3 class="mb-2 text-2xl font-extrabold text-white flex italic ">Vision <svg
-                                    class="w-8 h-8 ml-2" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
-                                    </path>
-                                </svg></h3>
-                            <h3 class="mb-2 text-1xl font-extrabold text-white">A leading ASEAN university actively
-                                committed to the total development of people for a globally sustainable environment
-                                and a humane society.</h3>
+                        <div>
+                            <figure class="mx-auto max-w-screen-md text-center">
+                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600"
+                                    viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
+                                        fill="currentColor"></path>
+                                </svg>
+                                <blockquote>
+                                    <p class="text-2xl italic font-medium text-gray-900 dark:text-white">A leading ASEAN
+                                        university actively
+                                        committed to the total development of people for a globally sustainable
+                                        environment
+                                        and a humane society.</p>
+                                </blockquote>
+                                <figcaption class="flex justify-center items-center mt-6 space-x-3">
+
+                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
+                                        <cite class="pr-3 font-medium text-gray-900 dark:text-white">Vision</cite>
+
+                                    </div>
+                                </figcaption>
+                            </figure>
                         </div>
 
                     </div>
@@ -154,9 +198,9 @@ defineProps({
 
 
                     <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-6 md:gap-12 md:space-y-0">
-                        <div class="  p-2 drop-shadow-sm" v-for="(colleges, key) in usePage().props.value.colleges" :key="key">
-                            <img class="grayscale  w-25 "
-                                :src=colleges.logo>
+                        <div class="  p-2 drop-shadow-sm" v-for="(colleges, key) in usePage().props.value.colleges"
+                            :key="key">
+                            <img class="grayscale  w-25 " :src=colleges.logo>
                         </div>
                     </div>
 
@@ -173,17 +217,20 @@ defineProps({
                         <!-- <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p> -->
                     </div>
                     <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-2 md:gap-12 md:space-y-0">
-                        <div v-for="(announcement, key) in usePage().props.value.announcements" :key="key" class="bg-white p-2 rounded drop-shadow-sm">
+                        <div v-for="(announcement, key) in usePage().props.value.announcements" :key="key"
+                            class="bg-white p-2 rounded drop-shadow-sm">
 
                             <div class="bg-gray-400 flex p-0 items-center">
                                 <h3
                                     class="border-double border-4 border-white text-xl px-4 max-w-[20rem] bg-green-800 font-bold text-white">
                                     {{ announcement.title}}</h3>
-                                <h3 class="ml-4 text-sm font-bold text-white">{{ date_conversion_complete(announcement.created_at)}}</h3>
+                                <h3 class="ml-4 text-sm font-bold text-white">{{
+                                date_conversion_complete(announcement.created_at)}}</h3>
                             </div>
 
                             <p class="text-gray-500 mb-2 mt-6">{{announcement.content}}</p>
-                            <p class="text-sm font-bold text-green-800">Posted by : Admin - {{ announcement.user.name }}</p>
+                            <p class="text-sm font-bold text-green-800">Posted by : Admin - {{ announcement.user.name }}
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -196,13 +243,15 @@ defineProps({
                         <!-- <p class="text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p> -->
                     </div>
                     <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-                        <div v-for="(events, key) in usePage().props.value.events" :key="key" class="bg-white p-2 rounded drop-shadow-sm">
+                        <div v-for="(events, key) in usePage().props.value.events" :key="key"
+                            class="bg-white p-2 rounded drop-shadow-sm">
 
                             <div class="bg-gray-400 flex p-0 items-center">
                                 <h3
                                     class="border-double border-4 border-white text-xl px-4 max-w-[8rem] bg-green-800 font-bold text-white">
                                     {{date_conversion(events.created_at)}}</h3>
-                                <h3 class="ml-4 text-sm font-bold text-white">{{date_conversion(events.from)}} to {{date_conversion(events.to)}}</h3>
+                                <h3 class="ml-4 text-sm font-bold text-white">{{date_conversion(events.from)}} to
+                                    {{date_conversion(events.to)}}</h3>
                             </div>
 
                             <p class="text-gray-500 mb-2 ">{{events.content}}</p>
