32cdf3334359e4811b16f6457b4b1fb34b13a28d
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-10-21 15:24:06 +0800

    remove dark mode in welcom

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -93,14 +93,14 @@ defineProps({
                         <div class="">
 
                             <figure class="mx-auto max-w-screen-md text-center">
-                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600"
+                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400"
                                     viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
                                         fill="currentColor"></path>
                                 </svg>
                                 <blockquote>
-                                    <p class="text-2xl italic font-medium text-gray-900 dark:text-white">Section 2 of
+                                    <p class="text-2xl italic font-medium text-gray-900">Section 2 of
                                         Republic Act No. 4498
                                         mandates that Central Mindanao University “shall provide programs of instruction
                                         at
@@ -115,8 +115,8 @@ defineProps({
                                 </blockquote>
                                 <figcaption class="flex justify-center items-center mt-6 space-x-3">
 
-                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                                        <cite class="pr-3 font-medium text-gray-900 dark:text-white">Mandate</cite>
+                                    <div class="flex items-center divide-x-2 divide-gray-500">
+                                        <cite class="pr-3 font-medium text-gray-900">Mandate</cite>
 
                                     </div>
                                 </figcaption>
@@ -125,14 +125,14 @@ defineProps({
                         </div>
                         <div class="">
                             <figure class="mx-auto max-w-screen-md text-center">
-                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600"
+                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400"
                                     viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
                                         fill="currentColor"></path>
                                 </svg>
                                 <blockquote>
-                                    <p class="text-2xl italic font-medium text-gray-900 dark:text-white">To advance the
+                                    <p class="text-2xl italic font-medium text-gray-900">To advance the
                                         frontier of
                                         knowledge through internationalization of education and equitable access to
                                         quality
@@ -141,8 +141,8 @@ defineProps({
                                 </blockquote>
                                 <figcaption class="flex justify-center items-center mt-6 space-x-3">
 
-                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                                        <cite class="pr-3 font-medium text-gray-900 dark:text-white">Mission</cite>
+                                    <div class="flex items-center divide-x-2 divide-gray-500 ">
+                                        <cite class="pr-3 font-medium text-gray-900">Mission</cite>
 
                                     </div>
                                 </figcaption>
@@ -150,14 +150,14 @@ defineProps({
                         </div>
                         <div>
                             <figure class="mx-auto max-w-screen-md text-center">
-                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400 dark:text-gray-600"
+                                <svg aria-hidden="true" class="mx-auto mb-3 w-12 h-12 text-gray-400"
                                     viewBox="0 0 24 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z"
                                         fill="currentColor"></path>
                                 </svg>
                                 <blockquote>
-                                    <p class="text-2xl italic font-medium text-gray-900 dark:text-white">A leading ASEAN
+                                    <p class="text-2xl italic font-medium text-gray-900">A leading ASEAN
                                         university actively
                                         committed to the total development of people for a globally sustainable
                                         environment
@@ -165,8 +165,8 @@ defineProps({
                                 </blockquote>
                                 <figcaption class="flex justify-center items-center mt-6 space-x-3">
 
-                                    <div class="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
-                                        <cite class="pr-3 font-medium text-gray-900 dark:text-white">Vision</cite>
+                                    <div class="flex items-center divide-x-2 divide-gray-500 ">
+                                        <cite class="pr-3 font-medium text-gray-900 ">Vision</cite>
 
                                     </div>
                                 </figcaption>
