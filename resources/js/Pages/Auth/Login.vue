cb0dd9a50ea532bf4dad768277f60faf2fff6781
MrRegei <39606446+MrRegei@users.noreply.github.com>
2022-10-27 13:43:58 +0800

    FE
    FEFE

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -30,9 +30,10 @@ const submit = () => {
 </script>
 
 <template>
+
     <Head title="Log in" />
 
-    <AuthenticationCard>
+    <!-- <AuthenticationCard>
         <template #logo>
             <AuthenticationCardLogo />
         </template>
@@ -45,27 +46,14 @@ const submit = () => {
             <InputError class="mt-2 mb-4" :message="form.errors.custom" />
             <div>
                 <InputLabel for="email" value="Email" />
-                <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    required
-                    autofocus
-                />
+                <TextInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required autofocus />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="password" value="Password" />
-                <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="current-password"
-                />
+                <TextInput id="password" v-model="form.password" type="password" class="mt-1 block w-full" required
+                    autocomplete="current-password" />
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 
@@ -77,8 +65,9 @@ const submit = () => {
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
+                <Link v-if="canResetPassword" :href="route('password.request')"
+                    class="underline text-sm text-gray-600 hover:text-gray-900">
+                Forgot your password?
                 </Link>
 
                 <PrimaryButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
@@ -86,5 +75,80 @@ const submit = () => {
                 </PrimaryButton>
             </div>
         </form>
-    </AuthenticationCard>
+    </AuthenticationCard> -->
+    <div>
+
+        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+            {{ status }}
+        </div>
+
+        <form @submit.prevent="submit">
+            
+            <div
+                class="flex  mt-[10vmin] w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl">
+
+                <div class="hidden bg-cover lg:block lg:w-1/2"
+                    style="background-image: url('https://devops.cmu.edu.ph/formlinks/assets/dist/assets/img/backgrounds/main_gate.jpg');">
+                </div>
+                
+                <div class="w-full px-6 py-8 md:px-8 lg:w-1/2">
+                    <div class="flex justify-center mb-2"><AuthenticationCardLogo /></div>
+                    <h2 class="text-2xl font-semibold text-center text-gray-700 dark:text-white">
+                        ALUMNI PORTAL
+                    </h2>
+
+                    <p class="text-xl text-center text-gray-600 dark:text-gray-200">
+                        Welcome back!
+                    </p>
+
+
+
+                    <div class="mt-4">
+                        <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                            for="LoggingEmailAddress">Email Address</label>
+                        <input id="email" v-model="form.email" type="email" required autofocus
+                            class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" />
+                        <InputError class="mt-2" :message="form.errors.email" />
+                    </div>
+
+                    <div class="mt-4">
+                        <div class="flex justify-between">
+                            <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                                for="loggingPassword">Password</label>
+                            <a v-if="canResetPassword" :href="route('password.request')"
+                                class="text-xs text-gray-500 dark:text-gray-300 hover:underline">Forget
+                                Password?</a>
+                        </div>
+
+                        <input id="password" v-model="form.password"
+                            class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                            type="password" />
+                        <InputError class="mt-2" :message="form.errors.password" />
+                        <InputError class="mt-2 mb-4" :message="form.errors.custom" />
+                    </div>
+                    <div class="block mt-4">
+                        <label class="flex items-center">
+                            <Checkbox v-model:checked="form.remember" name="remember" />
+                            <span class="ml-2 text-sm text-gray-600">Remember me</span>
+                        </label>
+                    </div>
+                    <div class="mt-8">
+                        <button :disabled="form.processing"
+                            class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-300 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                            Login
+                        </button>
+                    </div>
+
+                    <div class="flex items-center justify-between mt-4">
+                        <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+
+                        <a href="#" class="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline">or sign
+                            up</a>
+
+                        <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
