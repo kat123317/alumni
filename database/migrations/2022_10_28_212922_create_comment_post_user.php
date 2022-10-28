f7d474b74965de0f40b73fb12fe4f4de73bf235e
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-10-28 22:26:49 +0800

    added migrations

---
+++ database/migrations/2022_10_28_212922_create_comment_post_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_post_user', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id');
+            $table->longText('content');
+            $table->json('details')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment_post_user');
+    }
+};
