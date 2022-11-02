36be072e10b1cccccf483fc039fa166955291e6b
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-11-02 21:20:15 +0800

    custom add on user notification table

--- database/migrations/2022_10_25_132022_create_user_notifications_table.php
+++ database/migrations/2022_10_25_132022_create_user_notifications_table.php
@@ -16,6 +16,9 @@ return new class extends Migration
         Schema::create('user_notifications', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
+            $table->foreignId('notification_owner');
+            $table->string('notification_type', 255);
+            $table->boolean('is_read');
             $table->string('title', 255);
             $table->longText('content');
             $table->json('details')->nullable();
