fd27e4e6146957ea77c21426751eb4998364480a
MrRegei <39606446+MrRegei@users.noreply.github.com>
2022-11-02 19:07:34 +0800

    FEFE
    FE

---
+++ resources/js/Pages/Socialmedia/Components/UserProfile.vue
@@ -0,0 +1,17 @@
+<script setup>
+import AppLayout from "@/Layouts/AppLayout.vue";
+import { Head, Link, useForm, usePage } from "@inertiajs/inertia-vue3";
+import { ref, onMounted, computed } from "vue";
+
+import moment from "moment";
+</script>
+
+<template>
+     <AppLayout title="UserProfile">
+        sadsa
+     </AppLayout>
+</template>
+
+<style>
+
+</style>
