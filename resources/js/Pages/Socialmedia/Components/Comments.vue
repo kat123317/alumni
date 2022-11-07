4de8cf40b67ed079abae0e8d4f2c8ff1bfd20903
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-11-07 21:30:59 +0800

    added like and unlike in specific comments

--- resources/js/Pages/Socialmedia/Components/Comments.vue
+++ resources/js/Pages/Socialmedia/Components/Comments.vue
@@ -146,6 +146,30 @@ const function_delete_comment = () => {
         }
     );
 };
+
+const like_data = useForm({
+    post_id: null,
+});
+const function_like_post = (index) => {
+    let post = usePage().props.value.post[index];
+    let pkey = post.details.like.indexOf(usePage().props.value.user.name);
+    like_data.post_id = post.id;
+    if (pkey == -1) {
+        like_data.post(route("socialmedia.like_post", [post.id]), {
+            preserveScroll: true,
+            onSuccess: () => {
+                like_data.reset();
+            },
+        });
+    } else {
+        like_data.post(route("socialmedia.unlike_post", [post.id]), {
+            preserveScroll: true,
+            onSuccess: () => {
+                like_data.reset();
+            },
+        });
+    }
+};
 </script>
 
 <template>
@@ -353,7 +377,10 @@ const function_delete_comment = () => {
                 </div>
 
                 <div class="py-4">
-                    <a class="inline-flex items-center" href="#">
+                    <div
+                        @click="function_like_post(key)"
+                        class="inline-flex items-center"
+                    >
                         <button
                             type="button"
                             class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:border-blue-500"
@@ -373,9 +400,9 @@ const function_delete_comment = () => {
                                 />
                             </svg>
 
-                            120
+                            {{ post.details.like.length }}
                         </button>
-                    </a>
+                    </div>
                 </div>
                 <div class="relative">
                     <input
