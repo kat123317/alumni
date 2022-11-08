77f5fd0a343f12668a41c39da74909ada8a5fc9f
Arjie <arjie.bongcas@splitsecondsoftware.com>
2022-11-08 16:49:09 +0800

    done create question

--- resources/js/Pages/Administrator/Survey/Engine/Dropdown.vue
+++ resources/js/Pages/Administrator/Survey/Engine/Dropdown.vue
@@ -1,6 +1,6 @@
 <script setup>
 defineProps({
-    modelValue: String,
+    modelValue: Number,
     choices: Array,
 });
 
@@ -12,7 +12,7 @@ defineEmits(["update:modelValue"]);
         @change="$emit('update:modelValue', $event.target.value)"
         class="bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-green-500 focus:bg-white focus:ring-2 focus:ring-green-200 text-base outline-none text-gray-700 py-1 pl-3 leading-8 transition-colors duration-200 ease-in-out"
     >
-        <option value="" disabled>Select Choice</option>
+        <option value="0" disabled>Select Choice</option>
         <template v-for="(choice, i) in choices">
             <option :value="choice.value">
                 {{ choice.label }}
