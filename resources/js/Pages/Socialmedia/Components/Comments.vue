8924bcf1c0c40f166d2526e7864155c751ca995e
MrRegei <39606446+MrRegei@users.noreply.github.com>
2022-11-09 20:37:46 +0800

    FEFE
    FE

--- resources/js/Pages/Socialmedia/Components/Comments.vue
+++ resources/js/Pages/Socialmedia/Components/Comments.vue
@@ -187,63 +187,7 @@ function onSelectEmoji(emoji) {
 
 <template>
     <AppLayout title="Comments">
-        <div class="bg-gray-200">
-            <div
-                class="container flex items-center px-6 py-4 mx-auto overflow-y-auto whitespace-nowrap"
-            >
-                <a href="#" class="text-gray-600 dark:text-gray-200">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="w-5 h-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-                        />
-                    </svg>
-                </a>
-
-                <span
-                    class="mx-5 text-gray-500 dark:text-gray-300 rtl:-scale-x-100"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="w-5 h-5"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                    >
-                        <path
-                            fill-rule="evenodd"
-                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                            clip-rule="evenodd"
-                        />
-                    </svg>
-                </span>
-
-                <a
-                    href="#"
-                    class="flex items-center text-gray-600 -px-2 hover:underline"
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="w-6 h-6"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
-                        />
-                    </svg>
-
-                    <span class="mx-2">Comments</span>
-                </a>
-            </div>
-        </div>
+  
         <div
             v-if="postAlert"
             class="p-4 mb-4 border border-blue-300 rounded-lg bg-blue-50 dark:bg-blue-300"
