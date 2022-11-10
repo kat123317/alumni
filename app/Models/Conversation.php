dc73a6cdbc6edfb77321239f41e8f07462933446
Arjie <arjie.bongcas@splitsecondsoftware.com>
2022-11-10 16:29:35 +0800

    chat migration

---
+++ app/Models/Conversation.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conversation extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function user1()
+    {
+        return $this->belongsTo(User::class, 'user_id_1', 'id');
+    }
+
+    public function user2()
+    {
+        return $this->belongsTo(User::class, 'user_id_2', 'id');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+}
