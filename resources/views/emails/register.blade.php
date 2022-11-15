405756f8337a0a8474344c107250bc127d57e8f2
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-11-15 17:35:31 +0800

    added html in email

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,13 +1,24 @@
-@component('mail::message')
-# Introduction
 
-Dear {{$email}},
-You are now registered to CMU Alumni system.
-
-@component('mail::button', ['url' => env('APP_URL')])
-Visit site
-@endcomponent
-
-Thanks,<br>
-{{ config('app.name') }}
-@endcomponent
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <p></p>
+    <h1>Central Mindanao University Alumni System </h1>
+    <h3>Dear {{$email}},</h3>
+    <p>
+        You are now registered to CMU Alumni system.
+    </p>
+    <p>
+        If you are interested, you can redirect here
+    </p>
+    <p>
+    @component('mail::button', ['url' => env('APP_URL')])
+       <h2> Visit site </h2>
+    @endcomponent
+    </p>
+    <p>Thanks,</p>
+    <p>
+    {{ config('app.name') }}
+    </p>
+</body>
+</html>
