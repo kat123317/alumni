54ed56754cd3e8aa55d20acdb85142ef2c88c7de
Allan <allan.cabug-os@splitsecondsoftware.com>
2022-12-08 15:42:43 +0800

    updates

--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.vue
@@ -453,7 +453,6 @@ const remove_achievement = (key) => {
                 </a> -->
                 <a
                     class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-yellow-400 uppercase tracking-widest hover:bg-green-700 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring focus:ring-green-300 disabled:opacity-25 transition cursor-pointer"
-                    :class="{ 'opacity-25': form.processing }"
                     @click="function_add_honors()"
                 >
                     Add </a
