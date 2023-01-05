66337a77488f39a3d0845135f58ae8a4d8b2ed19
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-01-05 19:11:12 +0800

    added update in welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -92,6 +92,7 @@ defineProps({
                                 src="https://scontent.fmnl25-3.fna.fbcdn.net/v/t39.30808-6/305620435_493756352759167_5579240386512342003_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeEKAM5yrzsYH8VONLyaYOtJ5axEt3cQ-T3lrES3dxD5PavarxK6xZ120W-emYR9kItw_XaicBZzq3RsqJXpC4rE&_nc_ohc=B-XHQQXdH0kAX92Ux7R&_nc_ht=scontent.fmnl25-3.fna&oh=00_AfD-1d8glzoDHaKcmDVI2YRaHWiAcrL47V5orfyWMf1u6w&oe=63BB199C"
                                 class="mr-3 h-6 sm:h-9"
                                 alt="Cmu Logo"
+                                style="border-radius: 50%"
                             />
                             <span
                                 class="self-center text-xl font-bold whitespace-nowrap"
