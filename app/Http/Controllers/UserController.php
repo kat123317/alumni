f8124acf58ae209eb7f492abf1242640797ac019
Allan <allan.cabug-os@splitsecondsoftware.com>
2023-11-14 20:53:49 +0800

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -102,10 +102,11 @@ class UserController extends Controller
     }
 
 
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $input = $request->toArray();
 
-       
+
         Validator::make($input, [
             'fname' => ['required', 'string', 'max:255'],
             'lname' => ['required', 'string', 'max:255'],
@@ -116,71 +117,71 @@ class UserController extends Controller
             // 'user_type' => ['required', 'string', 'max:255'],
             'college_id' => ['required', 'integer'],
             'course_id' => ['required', 'integer'],
-            
+
             // 'date_of_birth' => ['required', 'date'],
-            
+
             // 'religion'=>['required', 'string'],
             // 'civil_status'=>['required'],
             // 'gender'=>['required'],
 
-            'address'=>['required', 'string'],
+            'address' => ['required', 'string'],
 
             // 'phone_number'=>['required', 'integer'],
             // 'telephone_number'=>['required', 'integer'],
 
             // 'current_work'=>['required'],
-            'year_graduated'=>['required'],
+            'year_graduated' => ['required'],
             // 'motto'=>['required'],
             // 'nickname' => ['required'], 
-            'region_of_origin'=> ['required', 'string'],
-            'province'=> ['required', 'string'],
+            'region_of_origin' => ['required', 'string'],
+            'province' => ['required', 'string'],
             'degree_graduated' => ['required'],
             'photos' => ['required'],
 
         ])->validate();
-        
+
 
         $images = array();
-        if($request->hasfile('photos')){
-            foreach($request->file('photos') as $photo){
-                $imageName = Str::random(40).'.'.$photo->extension();
-                $photo->move(public_path().'/images/verification/', $imageName); 
+        if ($request->hasfile('photos')) {
+            foreach ($request->file('photos') as $photo) {
+                $imageName = Str::random(40) . '.' . $photo->extension();
+                $photo->move(public_path() . '/images/verification/', $imageName);
                 array_push($images, $imageName);
             }
         }
 
         $user_details = array(
             //personal info
-            'address'=>$input['address'], 
-            'phone_number'=>$input['phone_number'], 
-            'telephone_number'=>$input['telephone_number'], 
-            'civil_status'=>$input['civil_status'], 
-            'gender'=>$input['gender'], 
-            'date_of_birth'=>$input['date_of_birth'], 
-            'region_of_origin'=>$input['region_of_origin'],
-            'province'=>$input['province'],
-            'religion'=>$input['religion'], 
+            'address' => $input['address'],
+            'phone_number' => $input['phone_number'],
+            'telephone_number' => $input['telephone_number'],
+            'civil_status' => $input['civil_status'],
+            'gender' => $input['gender'],
+            'date_of_birth' => $input['date_of_birth'],
+            'region_of_origin' => $input['region_of_origin'],
+            'province' => $input['province'],
+            'religion' => $input['religion'],
             //educational info
-            'degree_graduated'=> $input['degree_graduated'],
-            'course_id'=>$input['course_id'],
-            'honors_awards'=>$input['honors_awards']??[],
-            'professional_examination'=>$input['professional_examination'],
-            'current_work'=>$input['current_work'], 
-            'year_graduated'=>$input['year_graduated'], 
-            'motto'=>$input['motto']??'', 
-            'nickname'=>$input['nickname']??'',
-            'photos'=> $images??[]
+            'degree_graduated' => $input['degree_graduated'],
+            'course_id' => $input['course_id'],
+            'honors_awards' => $input['honors_awards'] ?? [],
+            'professional_examination' => $input['professional_examination'],
+            'current_work' => $input['current_work'],
+            'year_graduated' => $input['year_graduated'],
+            'motto' => $input['motto'] ?? '',
+            'nickname' => $input['nickname'] ?? '',
+            'photos' => $images ?? []
         );
 
-        $graduate = DB::table('graduates')->where('firstname',$input['fname'])->where('middlename',$input['mname'])->where('lastname',$input['lname'])->where('user_id', null)->first();
+        $graduate = DB::table('graduates')->where('firstname', $input['fname'])->where('middlename', $input['mname'])->where('lastname', $input['lname'])->where('user_id', null)->first();
         // dd($graduate);
 
-        if($graduate != null){
+        if ($graduate != null) {
             $user = User::create([
                 'fname' => $input['fname'],
                 'mname' => $input['mname'],
                 'lname' => $input['lname'],
-                'name' => $input['fname'].' '.$input['mname'].' '.$input['lname'],
+                'name' => $input['fname'] . ' ' . $input['mname'] . ' ' . $input['lname'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
                 'status' => 'approved',
@@ -191,19 +192,18 @@ class UserController extends Controller
                 'is_active' => 1,
             ]);
             $graduate_to_update = Graduate::find($graduate->id);
-            $graduate_to_update -> update([
+            $graduate_to_update->update([
                 'user_id' => $user->id
             ]);
             return Inertia::render('Auth/Register', [
                 'message_notification' => 'You may login your account now.'
             ]);
-        }
-        else{
+        } else {
             $user = User::create([
                 'fname' => $input['fname'],
                 'mname' => $input['mname'],
                 'lname' => $input['lname'],
-                'name' => $input['fname'].' '.$input['mname'].' '.$input['lname'],
+                'name' => $input['fname'] . ' ' . $input['mname'] . ' ' . $input['lname'],
                 'email' => $input['email'],
                 'password' => Hash::make($input['password']),
                 'status' => 'pending',
@@ -213,13 +213,13 @@ class UserController extends Controller
                 'details' => $user_details,
                 'is_active' => 0,
             ]);
-    
+
             //insert notification for new registrating users
             Notification::create([
                 'user_id' => $user->id,
                 'title' => 'New user registration',
                 'user_type' => 'staff_admin',
-                'content' => 'User '.$user->name.' is pending for approval',
+                'content' => 'User ' . $user->name . ' is pending for approval',
                 'is_processed' => false
             ]);
             // return Redirect::route('login');
@@ -231,53 +231,57 @@ class UserController extends Controller
         }
     }
 
-    public function deactivate_user(Request $request, $id){
+    public function deactivate_user(Request $request, $id)
+    {
         $user = User::find($id);
-        $user -> update([
-            'is_active'=>false
+        $user->update([
+            'is_active' => false
         ]);
         return Redirect::back();
     }
 
-    public function activate_user(Request $request, $id){
+    public function activate_user(Request $request, $id)
+    {
         $user = User::find($id);
-        $user -> update([
-            'is_active'=>true
+        $user->update([
+            'is_active' => true
         ]);
         return Redirect::back();
     }
 
-    public function assign_as_admin(Request $request, $id){
+    public function assign_as_admin(Request $request, $id)
+    {
         $user = User::find($id);
-        $user -> update([
-            'user_type'=> 'staff_admin'
+        $user->update([
+            'user_type' => 'staff_admin'
         ]);
         return Redirect::back();
     }
 
-    public function update_expire(Request $request){
-     
+    public function update_expire(Request $request)
+    {
+
         $expire = Authentication::where('secret_code', $request->code)->first();
         // dd($expire);
         if ($expire == null) {
             throw ValidationException::withMessages([
-                'expire_error'=> "Opps! Secret code do not match our records.",
+                'expire_error' => "Opps! Secret code do not match in our records.",
             ]);
-        }
-        else{
-            $expire -> update([
-                'due'=> $request->new_due,
+        } else {
+            $expire->update([
+                'due' => $request->new_due,
                 'key' => $request->new_key
             ]);
         }
-       
+
         return Redirect::back();
     }
 
-    public function remove_as_admin(Request $request, $id){
+    public function remove_as_admin(Request $request, $id)
+    {
         $user = User::find($id);
-        $user -> update([
-            'user_type'=> 'alumni'
+        $user->update([
+            'user_type' => 'alumni'
         ]);
         return Redirect::back();
     }
